828b0861446a89c94f9110f066b4286ef55e2ba4
Jasroop Singh <jasroop1304@gmail.com>
2023-12-14 15:56:51 +0530

    first comit

---
+++ count_freq.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+void count(int arr[],int n)
+{  
+    int i = 0;
+    while(i < n)
+    {
+        if(arr[i] <= 0)
+         {  i+=1;
+            continue;
+         }
+        
+        int eI=arr[i]-1;
+        if(arr[eI]>0)
+        {
+            arr[i]=arr[eI];
+            arr[eI]=-1;
+        }
+        else { arr[eI]-=1;
+          arr[i]=0;
+          i+=1;
+        }
+    }
+}
+int main()
+{   int arr[]={2,3,3,2,5};
+    count(arr,5);
+    for(int i=0;i<5;i++)
+       cout<<"Count of "<<i+1<<" is "<<abs(arr[i])<<"\n";
+    return 0;
+}
